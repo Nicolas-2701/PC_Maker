d9229c560e055752938f636765afc01305f04984
Nicolas-2701 <ngalvao388@gmail.com>
2025-03-21 07:04:10 -0300

    add - spring

---
+++ src/test/java/com/example/PC_Maker/PcMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PC_Maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
